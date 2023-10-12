c503dc5971313273cbd75426c9920f64c843ba58
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-10-12 12:57:48 +0530

    feat: Added Routing code for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+// -------------------------------
+// IMPORTS
+// -------------------------------
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// -------------------------------
+// COMPONENTS
+// -------------------------------
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />,
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={route} />;
 };
 
 export default App;
