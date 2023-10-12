3eae7b5d169d9c840a1a091ca70b6c461f6a5492
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-10-12 17:06:55 +0530

    perf: Added a Component which handle App layout for whole app  routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,34 @@ import Home from './ui/Home';
 import Menu from './features/menu/Menu';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import Cart from './features/cart/Cart';
 
 const route = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  {
-    path: '/order/new',
-    element: <CreateOrder />,
-  },
-  {
-    path: '/order/:orderId',
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <>
+      <Link to='/'>React Fast Food</Link>
+    </>
+  );
+};
+
+export default Header;
