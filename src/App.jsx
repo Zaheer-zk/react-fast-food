9944bc1b49d5f1bd44c6f5e3be80a073b00aea59
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-10-16 19:15:51 +0530

    perf: get order data and search order by id.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
 import Cart from './features/cart/Cart';
@@ -40,6 +40,7 @@ const route = createBrowserRouter([
       {
         path: '/order/:orderId',
         element: <Order />,
+        loader: orderLoader,
       },
     ],
   },
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SearchOrder from './SearchOrder';
 
 const Header = () => {
   return (
     <>
       <Link to='/'>React Fast Food</Link>
+      <SearchOrder />
     </>
   );
 };
